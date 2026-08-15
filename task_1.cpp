cadb80ba797aebb984c06383c2dca4c86b0400f3
Karinka <karina.rogalevich@mail.ru>
2026-08-15 17:02:52 +0200

    Übung 1

---
+++ task_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+    int age;
+
+    std::cin >> name >> age;
+
+    std::cout << name << std::endl;
+    std::cout << age << std::endl;
+
+    return 0;
+}
